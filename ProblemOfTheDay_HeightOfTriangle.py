cf8970570c32acf5b0114c85ac5d7ebc1cb71dd9
hind-alaraimi <h.al3rimi@gmail.com>
2025-04-22 13:06:03 +0400

    Add files via upload

--- ProblemOfTheDay_HeightOfTriangle.py
+++ ProblemOfTheDay_HeightOfTriangle.py
@@ -1,24 +1,25 @@
 o = int(input("Enter number of white balls: "))
 x = int(input("Enter number of black balls: "))
-r=1
-space=50
-H=0
+r = 1  # row size
+H = 0  # triangle height
 
-for i in range(space):
-    full_row=1
-    for j in range(1,i + 2):
-        if(r%2==1) and (o>0) and (o>=r):
-            print("o",end=" ")
-            o-=1
-        elif(r%2==0) and (x>0) and(x>=r):
-            print("x",end=" ")
-            x-=1
-        else:
-            full_row=0
-            break
-    if(full_row!=0):
-        print("")
-        H+=1
-        r+=1
-    
+for i in range(100):
+    full_row = True
+
+    # Check if there are enough balls BEFORE printing
+    if (r % 2 == 1 and o >= r):  # white row
+        print("o " * r)
+        o -= r
+    elif (r % 2 == 0 and x >= r):  # black row
+        print("x " * r)
+        x -= r
+    else:
+        full_row = False
+
+    if full_row:
+        H += 1
+        r += 1
+    else:
+        break
+
 print(f"\nHeight of triangle: {H}")
